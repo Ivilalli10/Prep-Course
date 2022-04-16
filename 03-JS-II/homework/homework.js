a761d61cf518e07264bca6db15fcd3c159c43de9
ivialejandralalli@gmail.com <ivialejandralalli@gmail.com>
2022-04-16 17:55:13 -0300

    Homework 2 completo sin array

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -188,6 +188,12 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor===true){
+    return "Soy verdadero";
+  }
+  else{
+    return "Soy falso";
+  }
 
 }
 
@@ -201,13 +207,25 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero.toString().length === 3){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i= 0;
+  var m = numero;
+  do{
+    i=i + 1
+    m=m + 5
+  }while(i<8);
+  return m;
 }
 
 
